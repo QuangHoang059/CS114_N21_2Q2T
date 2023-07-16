06592c61f118cdb6a662247837b266efec4a739e
Quanghoang059 <quanghoanglevi@gmail.com>
2023-07-16 12:57:50 +0700

    dsf

--- init.py
+++ init.py
@@ -32,15 +32,16 @@ parameter = {
 
 
 def extract_keypoints(results):
-    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten(
-    ) if results.pose_landmarks else np.zeros(33*4)
-    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten(
-    ) if results.face_landmarks else np.zeros(468*3)
+    # pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten(
+    # ) if results.pose_landmarks else np.zeros(33*4)
+    # face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten(
+    # ) if results.face_landmarks else np.zeros(468*3)
     lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten(
     ) if results.left_hand_landmarks else np.zeros(21*3)
     rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten(
     ) if results.right_hand_landmarks else np.zeros(21*3)
-    return np.concatenate([pose, face, lh, rh])
+    # return np.concatenate([pose, face, lh, rh])
+    return np.concatenate([lh, rh])
 
 
 def mediapipe_detection(image, model):
@@ -54,10 +55,10 @@ def mediapipe_detection(image, model):
 
 
 def draw_landmarks(mp_holistic, mp_drawing, image, results):
-    mp_drawing.draw_landmarks(image, results.face_landmarks,
-                              mp_holistic.FACEMESH_TESSELATION)
-    mp_drawing.draw_landmarks(image, results.pose_landmarks,
-                              mp_holistic.POSE_CONNECTIONS)
+    # mp_drawing.draw_landmarks(image, results.face_landmarks,
+    #                           mp_holistic.FACEMESH_TESSELATION)
+    # mp_drawing.draw_landmarks(image, results.pose_landmarks,
+    #                           mp_holistic.POSE_CONNECTIONS)
     mp_drawing.draw_landmarks(image, results.left_hand_landmarks,
                               mp_holistic.HAND_CONNECTIONS)
     mp_drawing.draw_landmarks(image, results.right_hand_landmarks,
@@ -65,19 +66,19 @@ def draw_landmarks(mp_holistic, mp_drawing, image, results):
 
 
 def draw_styled_landmarks(mp_holistic, mp_drawing, image, results):
-    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
-                              mp_drawing.DrawingSpec(
-                                  color=(80, 110, 10), thickness=1, circle_radius=1),
-                              mp_drawing.DrawingSpec(
-                                  color=(80, 256, 121), thickness=1, circle_radius=1)
-                              )
+    # mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
+    #                           mp_drawing.DrawingSpec(
+    #                               color=(80, 110, 10), thickness=1, circle_radius=1),
+    #                           mp_drawing.DrawingSpec(
+    #                               color=(80, 256, 121), thickness=1, circle_radius=1)
+    #                           )
 
-    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
-                              mp_drawing.DrawingSpec(
-                                  color=(80, 22, 10), thickness=2, circle_radius=4),
-                              mp_drawing.DrawingSpec(
-                                  color=(80, 44, 121), thickness=2, circle_radius=2)
-                              )
+    # mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+    #                           mp_drawing.DrawingSpec(
+    #                               color=(80, 22, 10), thickness=2, circle_radius=4),
+    #                           mp_drawing.DrawingSpec(
+    #                               color=(80, 44, 121), thickness=2, circle_radius=2)
+    #                           )
 
     mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
                               mp_drawing.DrawingSpec(
