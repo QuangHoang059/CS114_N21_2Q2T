d0d5db32d7511b0c74f8ff45d7a8ae3380260023
Trinhf21522717 <ktrinh1032003@gmail.com>
2023-07-16 16:09:52 +0700

    fix

--- createmodel.py
+++ createmodel.py
@@ -1,5 +1,6 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
+from init import parameter
 
 
 # def create_model(actions):
@@ -15,7 +16,7 @@ from tensorflow.keras.layers import LSTM, Dense
 def create_model(actions):
     model = Sequential()
     model.add(LSTM(64, return_sequences=True,
-              activation='relu', input_shape=(30, 126)))
+              activation='relu', input_shape=(parameter["FPS"], 126)))
     model.add(LSTM(128, return_sequences=True, activation='relu'))
     model.add(LSTM(64, return_sequences=False, activation='relu'))
     model.add(Dense(64, activation='relu'))
--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import cv2
 from init import mediapipe_detection, draw_styled_landmarks, extract_keypoints, parameter
 import mediapipe as mp
 import numpy as np
-from creatmodel import create_model
+from createmodel import create_model
 from PIL import ImageFont, ImageDraw, Image
 actions = parameter["actions"]
 model = create_model(actions)
